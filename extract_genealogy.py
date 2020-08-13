facd0195f485ffa7627f7db3c23b2da66e0e98a6
Ville Tossavainen <ville.tossavainen@gmail.com>
2020-08-12 20:06:08 -0700

    Refact: Removed unused function

--- extract_genealogy.py
+++ extract_genealogy.py
@@ -220,51 +220,6 @@ def convert_date_dmy_to_ymd(date):
             print('Not a valid date: {}. Date not converted.'.format(date))
     return date
 
-
-def update_number_of_children(person_id, number_of_children):
-    """
-    Update number of children in a person
-
-    Args:
-        (integer) person_id - Person ID
-        (integer) number_of_children - Number of known children
-    Returns:
-        (int) updated_rows - How many rows updated
-    """
-
-    sql = """ UPDATE persons
-                SET number_of_children = {}
-                WHERE person_id = {}"""
-
-    if len(number_of_children) == 0:
-        number_of_children = 'NULL'
-
-    conn = None
-    updated_rows = 0
-    try:
-        # read database configuration
-        params = config()
-        # connect to the PostgreSQL database
-        conn = psycopg2.connect(**params)
-        # create a new cursor
-        cur = conn.cursor()
-        # execute the UPDATE  statement
-        cur.execute(sql.format(number_of_children, person_id))
-        # get the number of updated rows
-        updated_rows = cur.rowcount
-        # Commit the changes to the database
-        conn.commit()
-        # Close communication with the PostgreSQL database
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-
-    return updated_rows
-
-
 def update_comments(value_id, comment_type, comments):
     """
     Update a comment value in a person
